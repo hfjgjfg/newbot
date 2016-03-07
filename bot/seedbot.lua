696203069a69f80b1de39397c693dea6038da6ef
hfjgjfg <mohammadhaselmehri20202020@gmail.com>
2016-03-08 00:41:06 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -218,8 +218,8 @@ function create_config( )
     "owners",
     "arabic_lock",
     "Plugins",
-    "xyxy",
-    "spam",
+    "locklink",
+    "setrank",
     "mainfucker",
     "set",
     "get",
@@ -230,7 +230,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {150575718,82009553,183310418,188151898,19287716,103987264,134843111},--Sudo users
+    sudo_users = {150575718},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     
